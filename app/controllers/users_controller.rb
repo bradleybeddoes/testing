76176a9bb0ac1d89d44e03713864d4b45933e536
Bradley Beddoes <bradleybeddoes@gmail.com>
2024-06-12 20:36:45 +1000

    Second

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-	@users = User.find_by_sql("SELECT * FROM users WHERE name = '#{params[:name]}'")
+	@users = User.all
   end
 
   # GET /users/1 or /users/1.json
